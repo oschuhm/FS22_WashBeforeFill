1a54ecab7a1cacece0c2a7ac6f5d0c096c3a49e8
Oliver Schuhmacher <oschuhm@web.de>
2022-04-04 20:27:15 +0200

    Update washBeforeFill.lua
    works now with
    - combine
    - trailer
    - shovel

--- FS22_WashBeforeFill/washBeforeFill.lua
+++ FS22_WashBeforeFill/washBeforeFill.lua
@@ -14,84 +14,119 @@ end
 
 function washBeforeFill.registerEventListeners(vehicleType)
   SpecializationUtil.registerEventListener(vehicleType, "onUpdate", washBeforeFill)
-  SpecializationUtil.registerEventListener(vehicleType, "onLoad", washBeforeFill)
+  SpecializationUtil.registerEventListener(vehicleType, "onLoadFinished", washBeforeFill)
 end
 
-function washBeforeFill:onLoad(savegame)
+function washBeforeFill.registerFunctions(vehicleType)
+  SpecializationUtil.registerFunction(vehicleType, "addLittleBit", washBeforeFill.addLittleBit)
+end
+
+function washBeforeFill:onLoadFinished(savegame)
   self.setNodeDirtAmount = Utils.appendedFunction(self.setNodeDirtAmount, washBeforeFill.appendToWash)
 
   if self.stopTipping ~= nil then
-    self.stopTipping = Utils.appendedFunction(self.stopTipping, washBeforeFill.appendStopTipping)
+    --self.stopTipping = Utils.appendedFunction(self.stopTipping, washBeforeFill.appendStopTipping)
   end
 
-  if self.handleDischarge ~= nil then
-    self.handleDischarge = Utils.appendedFunction(self.handleDischarge, washBeforeFill.shovelHandleDischarge)
+  if self.handleDischargeOnEmpty ~= nil then
+    self.handleDischargeOnEmpty = Utils.appendedFunction(self.handleDischargeOnEmpty, washBeforeFill.handleDischargeOnEmpty)
   end
 
-  if self.setWorkMode ~= nil then
-    self.setWorkMode = Utils.appendedFunction(self.setWorkMode, washBeforeFill.setWorkMode)
+  if self.getCanDischargeToObject ~= nil then
+    self.getCanDischargeToObject = Utils.overwrittenFunction(self.getCanDischargeToObject, washBeforeFill.getCanDischargeToObject)
   end
 
-  self.lastDirtAmount = 0
+  if self.getCanDischargeToGround ~= nil then
+    self.getCanDischargeToGround = Utils.overwrittenFunction(self.getCanDischargeToGround, washBeforeFill.getCanDischargeToGround)
+  end
+
+  if self.spec_fillUnit ~= nil then
+    if self.spec_fillUnit.fillTypeChangeThreshold ~= nil then
+        self.spec_fillUnit.fillTypeChangeThreshold = 0
+    end
+  end
+
 end
 
 function washBeforeFill:onUpdate(dt)
 end
 
-function washBeforeFill:shovelHandleDischarge(myObject)
-    print("append shovelHandleDischarge")
-end
-
-function washBeforeFill:setWorkMode(myObject)
-    print("append setWorkMode")
-    print(" WorkMode: " .. self:getIsTurnedOn())
+function washBeforeFill:handleDischargeOnEmpty(myObject)
+    print("append handleDischargeOnEmpty")
+    self:setDischargeState(Dischargeable.DISCHARGE_STATE_OFF)
+    self:addLittleBit()
 end
 
 function washBeforeFill:appendStopTipping(myObject)
     print("appendStopTipping")
+    self:addLittleBit()
+end
 
-     if self:getFillUnits() ~= nil then
+function washBeforeFill:getCanDischargeToObject(superFunc,dischargeNode)
+    local myFillLevel = self:getFillUnitFillLevel(dischargeNode.fillUnitIndex)
+    if myFillLevel <= 1 then
+      self:setDischargeState(Dischargeable.DISCHARGE_STATE_OFF)
+      return false
+    end
+
+    return superFunc(self, dischargeNode)
+
+end
+
+function washBeforeFill:getCanDischargeToGround(superFunc,dischargeNode)
+    local myFillLevel = self:getFillUnitFillLevel(dischargeNode.fillUnitIndex)
+    if myFillLevel <= 1 then
+      self:setDischargeState(Dischargeable.DISCHARGE_STATE_OFF)
+      return false
+    end
+
+    return superFunc(self, dischargeNode)
+
+end
+
+function washBeforeFill:addLittleBit()
+    print("addLittleBit")
+
+     if self:getFillUnits() ~= nil and self:getDirtAmount() > 0 then
 
         for _, fillUnitTable in pairs(self:getFillUnits()) do
             if fillUnitTable.fillLevel <= 0 then
-                print ("  we have to refill a little bit")
-                self:addFillUnitFillLevel(self:getOwnerFarmId(), fillUnitTable.fillUnitIndex, 0.001, fillUnitTable.lastValidFillType, ToolType.UNDEFINED, nil)
+                print ("  we have to refill a little bit " .. g_fillTypeManager:getFillTypeNameByIndex(fillUnitTable.lastValidFillType))
+                self:addFillUnitFillLevel(self:getOwnerFarmId(), fillUnitTable.fillUnitIndex, 1, fillUnitTable.lastValidFillType, ToolType.UNDEFINED, nil)
+                self:setFillUnitFillType(fillUnitTable.fillUnitIndex,fillUnitTable.lastValidFillType)
             else
-                print ("  there is still something in the trailer, nothing to do")
+                print ("  there is still something in the vehicle, nothing to do")
             end
         end
      end
 
 end
 
-function washBeforeFill.appendToWash(dirtAmount)
+function washBeforeFill.appendToWash(nodeData, dirtAmount, force)
 
-     dirtAmount.isCoverOpen = true
-     dirtAmount.fillTypesToIgnore = {
+     nodeData.isCoverOpen = true
+     nodeData.fillTypesToIgnore = {
        [g_fillTypeManager:getFillTypeByName("DIESEL").index] = true,
        [g_fillTypeManager:getFillTypeByName("DEF").index] = true,
        [g_fillTypeManager:getFillTypeByName("AIR").index] = true
        }
 
-
-     dirtAmount.currentDirtAmount = dirtAmount:getDirtAmount()
-
-     if dirtAmount:getFillUnits() ~= nil and dirtAmount.currentDirtAmount <= dirtAmount.lastDirtAmount then
+     if nodeData:getFillUnits() ~= nil and nodeData:getDirtAmount() == 0 then
         --DebugUtil.printTableRecursively(dirtAmount.fillTypesToIgnore,'ignore .. ',1,2)
-        for _, fillUnitTable in pairs(dirtAmount:getFillUnits()) do
+        for _, fillUnitTable in pairs(nodeData:getFillUnits()) do
 
-            if dirtAmount.fillTypesToIgnore[fillUnitTable.fillType] == nil then
+            if nodeData.fillTypesToIgnore[fillUnitTable.fillType] == nil then
 
                 -- read cover status
-                if dirtAmount.spec_cover ~= nil then
-                    if dirtAmount.spec_cover.hasCovers and dirtAmount.spec_cover.state == 0 then
-                        dirtAmount.isCoverOpen = false
+                if nodeData.spec_cover ~= nil then
+                    if nodeData.spec_cover.hasCovers and nodeData.spec_cover.state == 0 then
+                        nodeData.isCoverOpen = false
                     end
                 end
 
-                if fillUnitTable.fillLevel > 0 and dirtAmount.isCoverOpen then
+                if fillUnitTable.fillLevel > 0 and nodeData.isCoverOpen then
                     print ("  cleaning process active")
-                    dirtAmount:addFillUnitFillLevel(dirtAmount:getOwnerFarmId(), fillUnitTable.fillUnitIndex, -0.1, fillUnitTable.fillType, ToolType.UNDEFINED, nil)
+                    nodeData:addFillUnitFillLevel(nodeData:getOwnerFarmId(), fillUnitTable.fillUnitIndex, -1, fillUnitTable.fillType, ToolType.UNDEFINED, nil)
                 end
 
                 if fillUnitTable.fillLevel <= 0 then
@@ -100,7 +135,5 @@ function washBeforeFill.appendToWash(dirtAmount)
             end
         end
      end
-
-     dirtAmount.lastDirtAmount = dirtAmount.currentDirtAmount
 
 end
